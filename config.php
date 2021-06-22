32da71f71057f5928fa866213c4d2e781c31f5b4
Ridho Alfian <38149243+ridhoalfian@users.noreply.github.com>
2021-06-22 14:01:34 +0800

    Update config.php

--- config.php
+++ config.php
@@ -6,11 +6,11 @@ if (!version_compare(PHP_VERSION, '5.5.0', '>=')) {
 define('DBHOST', 'localhost');
 define('DBPORT', '3306');
 define('DBUSER', 'root');
-define('DBPASS', 'basoro');
-define('DBNAME', 'mlite');
+define('DBPASS', '');
+define('DBNAME', 'rshd_sik');
 
 // URL Webapps
-define('WEBAPPS_URL', 'http://localhost/Khanza-Lite/webapps');
+define('WEBAPPS_URL', 'http://localhost/Khanza-Lite/');
 define('WEBAPPS_PATH', BASE_DIR . '/webapps');
 
 // Admin cat name
@@ -43,6 +43,6 @@ define('BASIC_MODULES', serialize([
 ]));
 
 // Developer mode
-define('DEV_MODE', false);
+define('DEV_MODE', true);
 
 ?>
